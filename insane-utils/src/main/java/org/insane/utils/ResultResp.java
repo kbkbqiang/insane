a5712ca1bd6ec5f67abc5b85d1682d7ec97080d1
kbkbqiang <89085440@qq.com>
2017-08-23 15:29:51 +0800

    init insane-utils

---
+++ insane-utils/src/main/java/org/insane/utils/ResultResp.java
@@ -0,0 +1,26 @@
+package org.insane.utils;
+
+/**
+ * Created by wgc on 2016/5/24.
+ * desc:swagger返回消息实体
+ */
+public class ResultResp {
+    private String result;
+    private String body;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
---
+++ insane-utils/src/main/java/org/insane/utils/excel/TimeFormat.java
@@ -0,0 +1,32 @@
+package org.insane.utils.excel;
+
+import java.util.Date;
+
+import org.insane.utils.DateUtils;
+import org.springframework.util.StringUtils;
+
+
+/**
+ * 日期格式化，按传入的格式化字符串formatStr进行格式化
+ * 
+ * @author jiangping
+ * 
+ */
+@SuppressWarnings("rawtypes")
+public class TimeFormat implements DataFormat {
+
+	@Override
+	public String format(Object value, Object t, Object formatStr) {
+		if (null == value) {
+			return "";
+		}
+		if (value instanceof Date) {
+			if (StringUtils.isEmpty(formatStr)) {
+				formatStr = DateUtils.SHOW_DATE_FORMAT;
+			}
+			return DateUtils.format((Date) value, (String) formatStr);
+		}
+		return value.toString();
+	}
+
+}
